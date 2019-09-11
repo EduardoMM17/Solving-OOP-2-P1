d5b307cd91df7a807c40e378b7e4e1e86649f573
Marcuss17 <eduardomm171@gmail.com>
2019-09-11 14:49:32 -0500

    Adding main.cpp and CMakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/Solving_OOP_2_P1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Solving_OOP_2_P1.dir/main.cpp.obj"
+  "Solving_OOP_2_P1.pdb"
+  "Solving_OOP_2_P1.exe"
+  "Solving_OOP_2_P1.exe.manifest"
+  "libSolving_OOP_2_P1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Solving_OOP_2_P1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
